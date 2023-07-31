f1eeed47b0516eec928092e40fbad8c1d8aaf7ba
Kaan Karan <139341419+Kaan-Bey@users.noreply.github.com>
2023-07-31 20:32:13 +0300

    second sout

--- src/gitHubDay2.java
+++ src/gitHubDay2.java
@@ -3,5 +3,7 @@ public class gitHubDay2 {
     public static void main(String[] args) {
 
         System.out.println("Hello again gitHub");
+
+        System.out.println("I sent to new code");
     }
 }
